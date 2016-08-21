c9572f833352587efdaaf5f26e8fde2ee95bf71b
emanbuc <emanuele.buchicchio@informaticamente.it>
2016-08-21 17:17:11 +0200

    Working demo UWP application: send message OK, receive message OK.

---
+++ IotHubSample/IotHubSample/HubConfig.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IotHubSample
+{
+    /// <summary>
+    /// Add thi file to git repository with placeholder text and then
+    /// Add this file to .gitignore to avoid sharing secret keys
+    /// </summary>
+    internal class HubConfig
+    {
+        internal const string IOT_HUB_CONNECTION_STRING = "Replace this string with your device connection string from Device Explorer ";
+    }
+}
---
+++ IotHubSample/IotHubSample/MainPage.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.Devices.Client;
+using Windows.UI.Core;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace IotHubSample
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {        
+        public MainPage()
+        {
+            this.InitializeComponent();
+            ReceiveDataFromAzure();
+            SendDataToAzure("Application startup");
+        }
+
+        private void ClickMe_Click(object sender, RoutedEventArgs e)
+        {
+            this.HelloMessage.Text = "Hello, Windows 10 IoT Core!";
+            // send message to IOT Hub
+            SendDataToAzure("Button clicked");
+        }
+
+        private async Task SendDataToAzure(String textMessage)
+        {
+            DeviceClient deviceClient = DeviceClient.CreateFromConnectionString(HubConfig.IOT_HUB_CONNECTION_STRING, TransportType.Http1);
+
+            var msg = new Message(Encoding.UTF8.GetBytes(textMessage));
+
+            await deviceClient.SendEventAsync(msg);
+        }
+
+        public async Task ReceiveDataFromAzure()
+        {
+            DeviceClient deviceClient = DeviceClient.CreateFromConnectionString(HubConfig.IOT_HUB_CONNECTION_STRING, TransportType.Http1);
+
+            Message receivedMessage;
+            string messageData;
+
+            while (true)
+            {
+                receivedMessage = await deviceClient.ReceiveAsync();
+
+                if (receivedMessage != null)
+                {
+                    messageData = Encoding.ASCII.GetString(receivedMessage.GetBytes());
+
+
+                    // Report the change to the GUI (async)
+                    var task = Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+                    {
+                        TxtReceivedMsg.Text = messageData;
+                    });
+
+                    
+                    await deviceClient.CompleteAsync(receivedMessage);
+                }
+            }
+        }
+    }
+}
